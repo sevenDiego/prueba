1e9f998c484b3c404497a4e1f674b2ea3b75d730
sevenDiego <72025089+sevenDiego@users.noreply.github.com>
2021-03-23 23:00:04 +0100

    Add files via upload

---
+++ ejemploPorValorYReferencia/PasoPorValorYReferencia1.java
@@ -0,0 +1,22 @@
+package ejemploPorValorYReferencia;
+
+/**
+ * El resultado es 5
+ * the following Java program that passes a primitive type to function.
+ * 
+ * We pass an int to the function “change()” and as a result the change in the value of that integer is not reflected in the main method.
+ * 
+ * Java creates a copy of the variable being passed in the method and then do the manipulations. 
+ * Hence the change is not reflected in the main method.
+ */ 
+public class PasoPorValorYReferencia1 {
+	public static void main(String[] args) { 
+        int x = 5; 
+        change(x); 
+        System.out.println(x); 
+    } 
+
+	public static void change(int x) { 
+        x = 10; 
+    } 
+}
---
+++ ejemploPorValorYReferencia/PasoPorValorYReferencia2.java
@@ -0,0 +1,36 @@
+package ejemploPorValorYReferencia;
+
+/**
+ * The changes are not reflected back if we change the object itself to refer some other location or object
+ * If we assign reference to some other location, then changes are not reflected back in main(). 
+ */ 
+public class PasoPorValorYReferencia2 {
+	public static void main(String[] args) { 
+        // t is a reference 
+        Test t = new Test(5); 
+  
+        // Reference is passed and a copy of reference 
+        // is created in change() 
+        change(t); 
+  
+        // Old value of t.x is printed 
+        System.out.println(t.x);
+    }
+    public static void change(Test t) { 
+        // We changed reference to refer some other location 
+    	t = new Test(6); 
+        // now any changes made to reference are not reflected 
+        // back in main 
+
+        // si comento esta linea anterior entonces si funcionaria como yo espero que seria que t.x valiera 10 en el main
+        // If we do not change the reference to refer some other object (or memory location), 
+        // we can make changes to the members and these changes are reflected back.
+        t.x = 10; 
+    } 
+}
+class Test { 
+    int x; 
+    Test(int i) { 
+    	x = i; 
+    } 
+} 
